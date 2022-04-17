802839927c447cbc9cd56c2b586e4aa3459e41f9
DanyloLipar <danilo.lipar2016@gmail.com>
2022-04-17 22:00:13 +0300

    Save

--- src/components/TodoCard/TodoCard.tsx
+++ src/components/TodoCard/TodoCard.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { Todo } from "../../types/Todo";
+import classNames from "classnames";
 import './TodoCard.scss';
 
 type Props = {
@@ -11,15 +12,40 @@ export const TodoCard: React.FC<Props> = ({
     todo,
     setTodos
 }) => {
+    const [checker, setChecker] = useState(false);
     const deleting = () => {
         const todosArr: Todo[] = JSON.parse(localStorage.getItem('todos') || '');
         localStorage.setItem('todos', JSON.stringify(todosArr.filter(el => el.id !== todo.id)));
         setTodos(JSON.parse(localStorage.getItem('todos') || ''));
     }
 
+    const alreadyDone = () => {
+        if (!checker) {
+            const wholeTodos: Todo[] = JSON.parse(localStorage.getItem('todos') || '');
+            const todosLength = wholeTodos.length - 1;
+            setChecker(true);
+        }
+
+        if (checker) {
+            setChecker(false);
+        }
+    }
+
     return (
         <div className="activity">
-            <p>{todo.description}</p>
+            <div className="activity__container container">
+                <label className="container__cheking">
+                    <input type="checkbox"
+                        onClick={alreadyDone}
+                    />
+                </label>
+                <p className={classNames({
+                    'striked': checker,
+                })}
+                >
+                    {todo.description}
+                </p>
+            </div>
             <button
                 className="activity__delete"
                 onClick={deleting}
